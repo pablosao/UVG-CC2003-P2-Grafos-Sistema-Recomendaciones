40133f0b506b046fc04ba604b2221f84c1cc06bd
Pablo Sao <pablo_sao@outlook.com>
2019-05-22 12:46:10 -0600

    actualizando base de datos para crear porpiedad de rango de presupuesto archivo de python crea ya base de datos

--- Recomendacion.py
+++ Recomendacion.py
@@ -1,6 +1,6 @@
-
+#!/usr/bin/env python
+# -*- coding: Windows-1252 -*-
 # -*- coding: cp1252 -*-
-# -*- coding: 850 -*-
 # -*- coding: utf-8 -*-
 
 """
@@ -12,22 +12,26 @@ Created on Tue May 21 19:02:48 2019
 from dbConnection import Connection
 from readIni import read_ini
 import os
+import sys
+
+
 
 database = read_ini("database.ini","neo4j")
 
 path = 'scriptCreate.graph'
-query = ""
+query = ''
 
 if os.path.isfile(path):
-    file = open(path,'r')
+    file = open(path,"r")
     if file.mode == "r":
+        
         query = file.read()
     
 print
 try:
     ejec = Connection(str(database["bolt"]),str(database["user"]),str(database["password"]))
 
-    ejec.createDB(query)
+    ejec.createDB(query.encode('raw_unicode_escape').decode('utf-8'))
     
     #datos = ejec.greeting("Templado")
     
