646239a178e4885d0a4c694bf7762e0ff60fcaeb
Pablo Sao <pablo_sao@outlook.com>
2019-05-24 21:33:09 -0600

    Creando Nodo nuevo

--- inLugarTuristico.py
+++ inLugarTuristico.py
@@ -14,16 +14,41 @@ class lugarTuristico(wx.Frame):
         
     def __init__(self, parent, title): 
    
-        super(lugarTuristico, self).__init__(parent, title = title,size = (320,450))
+        super(lugarTuristico, self).__init__(parent, title = title,size = (320,500))
         
         self.panel = wx.Panel(self) 
         box = wx.BoxSizer(wx.VERTICAL) 
         
         #Texto Inicial
-        self.titulo = wx.StaticText(self.panel,label = "Recomendacion" ,style = wx.ALIGN_CENTRE) 
+        self.titulo = wx.StaticText(self.panel,label = "Crear Nodo" ,style = wx.ALIGN_CENTRE) 
         box.Add(self.titulo, 0 , wx.EXPAND |wx.ALIGN_CENTER_HORIZONTAL |wx.ALL, 20) 
         
+        
+        #--------------------------------------------------------------------------
+        
+        #Configuración ComboBox para selección de Municipio
+        lbmunicipio = wx.StaticText(self.panel,label = "Seleccione Municipio",style = wx.ALIGN_CENTRE) 
+        box.Add(lbmunicipio,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5) 
+        
+         # Creando información de ComboBox con datos de la base de datos
+        self.MUNICIPIO = []
+        
+        query = "match (municipio:Municipio) return municipio.titulo order by municipio.titulo"
+        
+        dato = ControladorGrafo.ExecQuery(query)
+        
+        for record in dato:
+            self.MUNICIPIO.append(record[0])
                 
+        
+        self.cbmunicipio = wx.Choice(self.panel,choices = self.MUNICIPIO) 
+        box.Add(self.cbmunicipio,1,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5)
+        
+        
+        #--------------------------------------------------------------------------
+        
+        
+        
         #Configuración ComboBox para selección de Clima
         lbtipo_clima = wx.StaticText(self.panel,label = "Escoja Clima",style = wx.ALIGN_CENTRE) 
         box.Add(lbtipo_clima,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5) 
@@ -50,6 +75,11 @@ class lugarTuristico(wx.Frame):
         box.Add(self.lbtipo_viaje,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5) 
         
         
+        
+        #--------------------------------------------------------------------------
+        
+        
+        
         # Creando información de ComboBox con datos de la base de datos
         self.TIPO_VIAJE = []
         
@@ -71,6 +101,7 @@ class lugarTuristico(wx.Frame):
         box.Add(self.lbtipo_turismo,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5) 
         
         
+        #--------------------------------------------------------------------------
         
         # Creando información de ComboBox con datos de la base de datos
         self.TIPO_TURISMO = []
@@ -88,6 +119,8 @@ class lugarTuristico(wx.Frame):
         box.Add(self.cbtipo_turismo,1,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5)
         
         
+        #--------------------------------------------------------------------------
+        
         #Configuración ComboBox para selección de Presupuesto
         self.lbtipo_turismo= wx.StaticText(self.panel,label = "Tipo Presupuesto",style = wx.ALIGN_CENTRE) 
         box.Add(self.lbtipo_turismo,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5) 
@@ -102,10 +135,11 @@ class lugarTuristico(wx.Frame):
         box.Add(self.lbnombre_nodo,0,wx.EXPAND|wx.ALIGN_CENTER_HORIZONTAL|wx.ALL,5) 
         
         
-        self.t1 = wx.TextCtrl(self.panel)
-        box.Add(self.t1,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
+        self.tblugar_turistico = wx.TextCtrl(self.panel)
+        box.Add(self.tblugar_turistico,1,wx.EXPAND|wx.ALIGN_LEFT|wx.ALL,5) 
         
         
+        #--------------------------------------------------------------------------
         
         # Agregamos botón para consulta
         self.btconsulta = wx.Button(self.panel,-1,"Obtener Recomendación") 
@@ -117,7 +151,7 @@ class lugarTuristico(wx.Frame):
         
         self.cbpresupuesto.SetSelection(1)
         
-        self.btconsulta.Bind(wx.EVT_BUTTON,self.getConsulta) 
+        self.btconsulta.Bind(wx.EVT_BUTTON,self.createNode) 
         
         if(not self.cbtipo_clima.IsEmpty()):
             self.cbtipo_clima.SetSelection(0)
@@ -127,18 +161,75 @@ class lugarTuristico(wx.Frame):
         
         if(not self.cbtipo_turismo.IsEmpty()):
             self.cbtipo_turismo.SetSelection(0)
+        
+        if(not self.cbmunicipio.IsEmpty()):
+            self.cbmunicipio.SetSelection(0)
 		
         self.panel.SetSizer(box) 
         self.Centre() 
         self.Show() 
     
-    def getConsulta(self,event):
+    def createNode(self,event):
         
-        print("ingreso nodo")
+        control = True
+        clima = ""
+        tipo_viaje = ""
+        tipo_turismo = ""
+        presupuesto = ""
+        nombre_nodo = ""
         
+        #print(self.TIPO_CLIMA[self.cbtipo_clima.GetSelection()])
+        if(self.cbtipo_clima.GetSelection() < 0 and control):
+            control = control and False
+            wx.MessageBox('Debe seleccionar un Tipo de Clima', 'Falta Información', wx.OK | wx.ICON_INFORMATION)
+        else:
+            clima =  self.cbtipo_clima.GetString(self.cbtipo_clima.GetSelection())
+            
+        if(self.cbtipo_viaje.GetSelection() < 0 and control):
+            control = control and False
+            wx.MessageBox('Debe seleccionar un Tipo de Viaje', 'Falta Información', wx.OK | wx.ICON_INFORMATION)
+        else:
+            tipo_viaje =  self.cbtipo_viaje.GetString(self.cbtipo_viaje.GetSelection())
+            
+        if(self.cbtipo_turismo.GetSelection() < 0 and control):
+            control = control and False
+            wx.MessageBox('Debe seleccionar un Tipo de Turismo', 'Falta Información', wx.OK | wx.ICON_INFORMATION)
+        else:
+            tipo_turismo = self.cbtipo_turismo.GetString(self.cbtipo_turismo.GetSelection())
         
-   
+        if(self.cbpresupuesto.GetSelection() < 0 and control):
+            control = control and False
+            wx.MessageBox('Debe seleccionar un Presupuesto', 'Falta Información', wx.OK | wx.ICON_INFORMATION)
+        else:
+            presupuesto = self.cbpresupuesto.GetString(self.cbpresupuesto.GetSelection())
         
+        if(self.tblugar_turistico.GetValue() == ""):
+            control = control and False
+            wx.MessageBox('Debe seleccionar un el Nombre del Nodo', 'Falta Información', wx.OK | wx.ICON_INFORMATION)
+        else:
+            nombre_nodo = self.tblugar_turistico.GetValue()
+        
+        if(control):
+            print("ingreso nodo")
+            
+            # Verificamos si existe el nódo
+            query = 'match (turismo:Turismo) where turismo.Lugar =~ "(?i).*{0}.*" return turismo.Lugar'.format(nombre_nodo)
+            
+            datos = ControladorGrafo.ExecQuery(query)
+            
+            coincidencias = 0
+            for record in datos:
+                if(record[0] != ""):
+                    coincidencias += 1
+            
+            if(coincidencias == 0):
+                
+                #creamos nódo
+                nodo = nombre_nodo.replace(" ","").upper()
+                
+                query = 'CREATE (%s:Turismo {Lugar: "%s", Presupuesto: "%s"})' % (nodo,nombre_nodo,presupuesto)
+                ControladorGrafo.ExecQuery(query)
+                
         
 app = wx.App() 
 lugarTuristico(None,  'Ingreso de Lugar Turistico') 
